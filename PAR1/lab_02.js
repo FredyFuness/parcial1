f528bc8aa4803bb10592e08c220e6d4b61e41e21
Fredy Funes <136938441+FredyFuness@users.noreply.github.com>
2024-02-19 09:20:41 -0600

    Add files via upload
    ¿Utilizaron estilos de boostrap o personalizados? ¿Porqué decidieron hacerlo
    de esta forma?
    No, porque no lo vimos de forma necesario, sin embargo, pensamos en usarlo.
    ¿De qué manera utilizaron los listeners? Descríbanlos a continuación.
    Cuando hacemos click en la imagen del Capitan Gerardo Barrios.
    ¿Qué otro tipo de listeners creen que pueden agregar a su página y de qué
    manera lo implementarían?
    Evento Scroll: 
    Puedes usar eventos de scroll para realizar acciones cuando el usuario realiza desplazamiento en la página.

---
+++ PAR1/lab_02.js
@@ -0,0 +1,9 @@
+document.getElementById('box1').addEventListener('mouseover', function() {
+    this.innerHTML = "<p class='overlay-text'>José Gerardo Barrios fue un militar, estadista y político salvadoreño.</p>";
+});
+
+
+
+document.getElementById('box7').addEventListener('focus', function() {
+    this.innerHTML = "<p class='overlay-text'>De pequeño me gustaba.</p>";
+});
